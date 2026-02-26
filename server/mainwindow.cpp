5aec9711fc920696dc21744e06e3e85f5b43dd6b
leila-wilde <leila.wilde@laplateforme.io>
2026-02-26 22:55:07 +0000

    feat(server ui): initialize ServerManager in MainWindow & implement ui. Display connected clients in list widget with id, connect ServerManager signals to UI slots & add 'Chats' and 'Clients' panels with 'Disconnect All' button

--- server/mainwindow.cpp
+++ server/mainwindow.cpp
@@ -6,9 +6,28 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setupServer();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::newClientConnected(QTcpSocket *client)
+{
+    auto id = client->property("id").toInt();
+    ui->lstClients->addItem(QString("New client added: %1").arg(id));
+}
+
+void MainWindow::clientDisconnected(QTcpSocket *client)
+{
+
+}
+
+void MainWindow::setupServer()
+{
+    _server = new ServerManager();
+    connect(_server, &ServerManager::newClientConnected, this, &MainWindow::newClientConnected);
+    connect(_server, &ServerManager::clientDisconnected, this, &MainWindow::clientDisconnected);
 }
