682b80fef814dd0f730d09a143b7366542a30360
leila-wilde <leila.wilde@laplateforme.io>
2026-02-27 00:51:12 +0000

    feat(client): add ClientManager for TCP coomunication with server. \ - add TCP socket connection to server\ - handle connection, disconnection & data reception\ - provide methods for connecting and sending messages \ - emit signals for client-side event handling

---
+++ client/ClientManager.h
@@ -0,0 +1,30 @@
+#ifndef CLIENTMANAGER_H
+#define CLIENTMANAGER_H
+
+#include <QObject>
+#include <QTcpSocket>
+#include <QHostAddress>
+
+class ClientManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ClientManager(QHostAddress ip = QHostAddress::LocalHost, ushort port = 4500, QObject *parent = nullptr);
+    void connectToServer();
+    void sendMessage(QString message);
+
+signals:
+    void connected();
+    void disconnected();
+    void dataReceived(QByteArray data);
+
+private slots:
+    void readyRead();
+
+private:
+    QTcpSocket *_socket;
+    QHostAddress _ip;
+    ushort _port;
+};
+
+#endif // CLIENTMANAGER_H
